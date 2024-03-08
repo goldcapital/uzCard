ad61f2ba71e269a9e64d07ed7ec3c9a0a7631986
muham <muhammadabduraimov5@gamil.com>
2024-03-08 18:06:26 +0500

    uz card

---
+++ src/main/java/com/example/UzCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzCardServiceApplication.class, args);
+    }
+
+}
